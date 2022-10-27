9e5665fdb7349be480727dc55b14225f32c18f95
Roger <roger.chaves.rs2@gmail.com>
2022-10-26 23:17:53 -0300

    Adicionando rotas

---
+++ src/main/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Rotas from "./routes";
+
+import 'bootswatch/dist/darkly/bootstrap.css'
+import '../custom.css'
+
+class App extends React.Component {
+
+  render() {
+    return(
+      
+        <Rotas />
+      
+    )
+  }
+}
+
+export default App;
