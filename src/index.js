d6e0f954604472e120a10492adc874ded25951f2
Roger Chaves <roger.chaves.rs@gmail.com>
2023-01-12 22:38:32 -0300

    tudo comitado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './main/App';
+const cors = require('cors');
 
+App.use(cors());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
