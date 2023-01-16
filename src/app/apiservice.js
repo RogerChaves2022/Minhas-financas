7fc2fbf1004058600e1aa01c5a96ce50b4675172
Roger Chaves <roger.chaves.rs@gmail.com>
2023-01-16 20:35:42 -0300

    apenas origin e methods

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -2,10 +2,8 @@ import axios from 'axios'
 
 export const httpClient = axios.create({
     baseURL: "http://192.168.0.174:8080", withCredentials: true, headers: {                  
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Authorization", 
-        "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE" ,
-        "Content-Type": "application/json;charset=UTF-8"                   
+        "Access-Control-Allow-Origin": "*", 
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE"                 
     }
 })
 
